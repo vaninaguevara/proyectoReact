a90772199572f7596d68a958154e8bb4b3d2dcaa
vaniguevara <vaani.guevara@gmail.com>
2022-06-21 22:25:31 -0300

    Creacion del Header

---
+++ src/Components/Header.js
@@ -0,0 +1,26 @@
+import React from "react";
+// import {ShoppingCartIcon} from "@mui/icons-material/ShoppingCart";
+import "./Header.css";
+import logo from "./../emunah-logo.jpg";
+
+const Header = () => {
+    return(
+        <header>
+            <div className="nav-registro">
+                <a href="">Registrarme | </a>
+                <a href="">Iniciar sesión</a>
+            </div>
+            <div className="logo-fondo">
+                <img src={logo} alt="" />
+            </div> 
+            <nav>
+                <a href="">PRODUCTOS</a>
+                <a href="">SOBRE MI</a>
+                <a href="">CONTACTO</a>
+                <a href="">MI CARRITO</a>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
